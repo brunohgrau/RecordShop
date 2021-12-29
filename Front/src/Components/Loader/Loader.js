0bea4fc6d9a0fdeb76409ecb347d2b0a54c2efaa
brunohgrau <brunohgrau99@gmail.com>
2021-12-29 10:39:23 -0300

    Delete Front directory

--- Front/src/Components/Loader/Loader.js
+++
@@ -1,9 +0,0 @@
-const Loader = () => {
-  return (
-    <div class="flex justify-center items-center">
-      <div class="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
-    </div>
-  );
-};
-
-export default Loader;
